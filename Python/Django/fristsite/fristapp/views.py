42ecb457f0a56d17493bc278c31ed5eee61b825f
LTMana <30500660@qq.com>
2017-03-23 21:30:21 +0800

    creatdatabase

--- Python/Django/fristsite/fristapp/views.py
+++ Python/Django/fristsite/fristapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from fristapp.models import People
 # Create your views here.
-# def first_try(request):
-#     person=
+
+
+def first_try(request):
+    person = People(name='Spork', job="officer")
+    return
