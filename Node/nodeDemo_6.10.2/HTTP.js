dd037fb3e8fbb51d64746d7263992bf008fe6e42
LTMana <30500660@qq.com>
2017-05-22 19:11:01 +0800

    node demo

--- Node/nodeDemo_6.10.2/HTTP.js
+++ Node/nodeDemo_6.10.2/HTTP.js
@@ -1,21 +1,49 @@
 'use strict'
- const http = require('http')
-
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const querystring = require('querystring')
 http.createServer((requset, response) => {
-  response.write('htdfdfdsfdsfds')
-  response.end();
+
+  console.log(requset.url, requset.method)
+  let urlObjc = url.parse(requset.url, true)
+  if (urlObjc.pathname == '/htd' && requset.method == 'GET') {
+
+
+
+    response.write(urlObjc.pathname)
+    let dd = urlObjc.query
+    response.write(dd.ddd)
+    response.end();
+  } else if (urlObjc.pathname == '/hello' && requset.method == 'POST') {
+
+    let data = ''
+    response.on('data', (chunk) => {
+      console.log('ddddddddd')
+      data += chunk
+    })
+    response.on('end', () => {
+      console.log('endendend')
+      let obj = querystring.parse(data)
+
+      console.log('obj',obj)
+
+      response.end()
+    })
+
+
+
+
+  } else if (urlObjc.pathname == '/mac.html' && requset.method == 'GET') {
+
+    fs.readFile('./mac/index.html', 'utf8', (err, data) => {
+      console.log(err)
+      response.end(data)
+    })
+
+
+  }
+
+
 
 }).listen(3000);
-
-
-
-// server.on('requset',(requset,response)=>{
-//     console.log('dfdsfdsfdsfdsfds')
-//     response.write('htdfdfdsfdsfds')
-//     response.end();
-
-
-// })
-
-
-// server.listen(10625)
